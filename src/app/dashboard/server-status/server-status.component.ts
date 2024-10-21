25e1d0dcd405f810c7a34e5c644ef38f6a815944
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-21 12:45:31 +0530

    121. Introducing the Component Lifecycle: ngOnInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -11,19 +11,23 @@ export class ServerStatusComponent {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
   constructor(){
-    setInterval(() => {
-        const rnd = Math.random();
+    
+  }
 
-        if(rnd<0.5){
-          this.currentStatus = 'online';
-        }
-        else if(rnd<0.9){
-          this.currentStatus = 'offline';
-        }
-        else{
-          this.currentStatus = 'unknown';
-        }
-    }, 5000)
+  ngOnInit(){
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if(rnd<0.5){
+        this.currentStatus = 'online';
+      }
+      else if(rnd<0.9){
+        this.currentStatus = 'offline';
+      }
+      else{
+        this.currentStatus = 'unknown';
+      }
+  }, 5000)
   }
 
 }
