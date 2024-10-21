03bc454654c04bdbced38d82a3128f151012a1af
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-21 19:08:49 +0530

    138. Component Outputs: Repetition

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -14,9 +14,14 @@ export class NewTicketComponent implements AfterViewInit, OnInit {
 @ViewChild('form') private form ?: ElementRef<HTMLFormElement>; 
 // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+// @Output() add = new EventEmitter<{title: string, text: string}>();
+add = output<{title: string; text: string}>();
+
  onSubmit(title: string, ticketText: string){
-  console.log(title);
-  console.log(ticketText);
+  this.add.emit({
+    title: title,
+    text: ticketText
+  })
   this.form?.nativeElement.reset();
   // this.form()?.nativeElement.reset();
  }
